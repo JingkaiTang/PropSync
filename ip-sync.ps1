5dcb54e1f973486fe62e057b6d92d54bea0dc8f3
Jingkai Tang <jingkaitang@gmail.com>
2017-03-09 10:21:23 +0800

    add: ip-sync script

---
+++ ip-sync.ps1
@@ -0,0 +1,9 @@
+﻿$IP = "xxx"#cat .\ip
+$currentIP = ".y1xyx1y."#(Get-NetIPAddress -IPAddress 192.168.1.* | select IPAddress).IPAddress
+if (!$IP.Equals($currentIP)) {
+    $currentIP > .\ip
+    C:\Users\totran\AppData\Local\GitHub\shell.ps1
+    git add .\ip
+    git commit -m "update: ip"
+    git push 2> $null
+}
